2310f2a749ea1a0c30faa2b07cf5dfb9c0e49166
Dinesh065 <dc9359349132@gmail.com>
2024-07-12 21:01:46 +0530

    Solved Problems on Advance Pattern

--- Pr_2.cpp
+++ Pr_2.cpp
@@ -22,20 +22,52 @@ int main(){
     //     cout<<endl;
     // }
 
+    //0 1 Pattern
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=i;j++){
+    //         if((i+j)%2==0){
+    //             cout<<" 1";
+    //         }
+    //         else{
+    //             cout<<" 0";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    //Rhombus Pattern 
+    //     * * * *
+    //    * * * *
+    //   * * * *
+    //  * * * *
+    // * * * *
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=(n-i);j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=1;j<=n;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //Number Pattern
+    //     1
+    //    1 2
+    //   1 2 3
+    //  1 2 3 4
+    // 1 2 3 4 5
+
     for(int i=1;i<=n;i++){
+        for(int j=1;j<=(n-i);j++){
+            cout<<" ";
+        }
         for(int j=1;j<=i;j++){
-            if((i+j)%2==0){
-                cout<<" 1";
-            }
-            else{
-                cout<<" 0";
-            }
+            cout<<j<<" ";
         }
         cout<<endl;
     }
-
-
-
 
     return 0;
 }
